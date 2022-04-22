a7d203051cb2747e07b945a52c4b3db9a5d1ed0a
cozydevy <cozydevy@gmail.com>
2022-04-22 23:36:26 +0700

    read one api interrect

--- api/interact/read_one.php
+++ api/interact/read_one.php
@@ -39,7 +39,7 @@ $numotherdurg = count($data->otherdrug);
 
 // for ($i = 0; $i < $numdurg; $i++) {
 //     // array_push($iddrug,$data->drug[$i]->iddrug);
-  
+
 
 //     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 //         //     // extract row
@@ -69,49 +69,57 @@ $numotherdurg = count($data->otherdrug);
 // // echo json_encode($interact_arr);
 // echo json_encode($iddrug);
 $num = 1;
-$interact_arr=array();
-    $interact_arr["interact"]=array();
+$interact_arr = array();
+
 // // check if more than 0 record found
-if($num>0){
-  
+if ($num > 0) {
+
     // interact array
     for ($i = 0; $i < $numdurg; $i++) {
-        for ($j = 0; $j < $numotherdurg; $j++) {
-    $interact->iddrug = $data->drug[$i]->iddrug;
-    $interact->idotherdrug = $data->otherdrug[$j]->idotherdrug;
-    
-    $stmt = $interact->readOne();
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        // extract row
-        // this will make $row['name'] to
-        // just $name only
-        extract($row);
-  
-        $interact_item=array(
-            "id" => $id,
-            "iddrug" => $iddrug,
-            "idotherdrug" => $idotherdrug,
-            "summary" => $summary,
-            "severity" => $severity,
-            "documentation" => $documentation,
-            "clarification" => $clarification,
-            "reference" => $reference
-        );
-  
-        array_push($interact_arr["interact"], $interact_item);
-    }
-}
+        $interact->iddrug = $data->drug[$i]->iddrug;
+        $namedrug=(string)$interact->iddrug; 
 
-}
+        $interact_arr[$namedrug] = array();
+
+        for ($j = 0; $j < $numotherdurg; $j++) {
+           
+
+            $interact->idotherdrug = $data->otherdrug[$j]->idotherdrug;
+
+            $stmt = $interact->readOne();
+            // retrieve our table contents
+            // fetch() is faster than fetchAll()
+            // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['name'] to
+                // just $name only
+                extract($row);
+
+                $interact_item = array(
+                    "id" => $id,
+                    "iddrug" => $iddrug,
+                    "idotherdrug" => $idotherdrug,
+                    "summary" => $summary,
+                    "severity" => $severity,
+                    "documentation" => $documentation,
+                    "clarification" => $clarification,
+                    "reference" => $reference
+                );
+                array_push($interact_arr[$namedrug], $interact_item);
+
+            }
+        }
+    }
+    $arr = array();
+
+    $arr["interaction"] = $interact_arr;
+
     // set response code - 200 OK
     http_response_code(200);
-  
-    // show interact data in json format
-    echo json_encode($interact_arr);
 
+    // show interact data in json format
+    echo json_encode($arr);
 } else {
 
     // set response code - 404 Not found
